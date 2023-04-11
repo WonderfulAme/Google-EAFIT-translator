11e28d7bce7bf178d3e615972bac77dfb69cc63f
Samuel Suesca Ríos <96037106+Samsuesca@users.noreply.github.com>
2023-04-11 11:50:54 -0500

    interfaz

--- data_manager/adder.py
+++ data_manager/adder.py
@@ -2,13 +2,16 @@
 import os
 import streamlit as st
 from manager import add_translation_to_json_file
+from deep_translator import GoogleTranslator 
+	
+translator = GoogleTranslator(source='en', target='es')
 
 json_file_name = 'sentences.JSON'
 json_file_path = os.path.abspath(json_file_name)
 
 st.write("Aplicación de Streamlit para almacenar texto")
 text_input_ing = st.text_area("Escribe tu párrafo aquí en ingles:")
-text_input_spa = st.text_area("Escribe tu párrafo aquí en español:")
+text_input_spa = translator.translate(text_input_ing)
 
 if st.button("Guardar"):
     add_translation_to_json_file(text_input_ing, text_input_spa, json_file_path)
--- data_manager/manager.py
+++ data_manager/manager.py
@@ -1,10 +1,30 @@
 import json
 
-def add_translation_to_json_file(english_phrase, spanish_translation, json_file_path):
+def add_translation_to_json_file(english_phrase: str, spanish_translation: str, json_file_path: str) -> None:
+    """
+    Agrega una nueva entrada al archivo JSON ubicado en json_file_path.
+
+    Parámetros
+    ----------
+    english_phrase : str
+        La frase en inglés que se quiere agregar al archivo JSON.
+    spanish_translation : str
+        La traducción de la frase en español que se quiere agregar al archivo JSON.
+    json_file_path : str
+        La ruta absoluta del archivo JSON donde se quiere agregar la nueva entrada.
+
+    Retorno
+    -------
+    None
+    """
+
+    # Abrir el archivo JSON en modo lectura y cargar su contenido en la variable 'data'
     with open(json_file_path, 'r') as json_file:
         data = json.load(json_file)
 
+    # Agregar la nueva entrada al diccionario 'data'
     data[english_phrase] = spanish_translation
 
+    # Sobrescribir el archivo JSON con el nuevo contenido
     with open(json_file_path, 'w') as json_file:
         json.dump(data, json_file, indent=2)
